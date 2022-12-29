e6769db235c54468426ede6d490f9868d9efb2e6
Ankit Mishra <ankitmishra2108@gmail.com>
2022-12-29 23:37:14 +0530

    Getting user by their Unique id

--- routes/users.js
+++ routes/users.js
@@ -20,4 +20,12 @@ router.post('/', (req, res) => {
   res.send(`User with the name ${user.firstName} added to the database!`);
 });
 
+// Getting user by their id
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  
+  const foundUser = users.find((user) => user.id === id);
+  res.send(foundUser);
+})
+
 export default router;

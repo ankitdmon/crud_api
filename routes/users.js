fba75094b591040041128da90110cb922360cd14
Ankit Mishra <ankitmishra2108@gmail.com>
2022-12-29 23:24:35 +0530

    Proving Unique Id for each user using uuid

--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,9 @@
 import express from 'express';
+import { v4 as uuidv4 } from 'uuid';
 
 const router = express.Router();
 
-const users = [
-  {
-    firstName: 'Ankit',
-    lastName: 'Mishra',
-    Age: 21,
-  },
-  {
-    firstName: 'Apoorva',
-    lastName: 'Mishra',
-    Age: 21,
-  },
-];
+const users = [];
 
 // Every route will start with /users
 router.get('/', (req, res) => {
@@ -23,8 +13,10 @@ router.get('/', (req, res) => {
 router.post('/', (req, res) => {
   // console.log('Post Route Reached!');
   const user = req.body;
-  users.push(user);
-  
+
+  const userWithId = { ...user, id: uuidv4() };
+  users.push(userWithId);
+
   res.send(`User with the name ${user.firstName} added to the database!`);
 });
 

88d8a7e2cfafd8ae754e80a79f27657965a12f5a
Ankit Mishra <ankitmishra2108@gmail.com>
2022-12-29 23:52:21 +0530

    Deleting user by their id

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ import { v4 as uuidv4 } from 'uuid';
 
 const router = express.Router();
 
-const users = [];
+let users = [];
 
 // Every route will start with /users
 router.get('/', (req, res) => {
@@ -23,9 +23,17 @@ router.post('/', (req, res) => {
 // Getting user by their id
 router.get('/:id', (req, res) => {
   const { id } = req.params;
-  
+
   const foundUser = users.find((user) => user.id === id);
   res.send(foundUser);
-})
+});
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+
+  users = users.filter((user) => user.id != id);
+
+  res.send(`User with the id ${id} deleted from the data base!`);
+});
 
 export default router;
